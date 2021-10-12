989b15d7a84906145f1f427abec3cdad6d7d3897
F0rsaken <91898958+f0urs@users.noreply.github.com>
2021-10-12 12:38:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+function m1(){
+   var sp1 = document.getElementById("sp1");
+ sp1.className = "sp-1 sp-1-click";
+
+ var div1 = document.getElementById("div1");
+ div1.className = "div-1 i1";
+
+}
+
+
+function m3(){
+ var sp3 = document.getElementById("sp3");
+ sp3.className = "sp-1 sp-1-click";
+
+  var div1 = document.getElementById("div1");
+ div1.className = "div-1 i3";
+  
+}
+
+function m4(){
+   var sp4 = document.getElementById("sp4");
+ sp4.className = "sp-1 sp-1-click";
+
+  var div1 = document.getElementById("div1");
+ div1.className = "div-1 i4";
+  
+}
+
+function m5(){
+   var sp5 = document.getElementById("sp5");
+ sp5.className = "sp-1 sp-1-click";
+
+  var div1 = document.getElementById("div1");
+ div1.className = "div-1 i5";
+  
+}
+
+function m6(){
+   var sp6 = document.getElementById("sp6");
+ sp6.className = "sp-1 sp-1-click";
+
+  var div1 = document.getElementById("div1");
+ div1.className = "div-1 i6";
+  
+}
+
+function m7(){
+   var sp7 = document.getElementById("sp7");
+ sp7.className = "sp-1 sp-1-click";
+
+  var div1 = document.getElementById("div1");
+ div1.className = "div-1 i7";
+  
+}
+
